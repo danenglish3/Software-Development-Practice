2c65f13761311f19ded9dbcdb4bf105659d83960
Jordan Lamb <JordanLamb1998@gmail.com>
2018-08-29 10:51:06 +1200

    Basic login page layout completed and login.js scripts

---
+++ client/login.js
@@ -0,0 +1,13 @@
+const emailField = document.querySelector('#email_input');
+const passwordField = document.querySelector('#password_input');
+const loginButton = document.querySelector('#loginButton');
+
+let userPasswordInput = '';
+let userEmailInput = '';
+
+function getDetails() {
+    userPasswordInput = emailField.Value;
+    userEmailInput = emailField.Value;
+}   
+
+loginButton.onClick = getDetails();
