4da3eeefa707988cdf8b765903a323d8392a85b0
Suved <dwn2786@autuni.ac.nz>
2018-09-06 21:11:17 +1200

    Added and configured express middleware and view engine.
    Configured multer, set public as the static files folder and
    defined the views folder.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 /* Dependencies */
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 
 /* Creating and running an express server */
 const app = express(); // Initialise express
@@ -8,10 +10,13 @@ const PORT = 3000; // Specify a network port
 
 // Set view engine to EJS
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 // Provide Express with middleware
+app.use(express.static(path.join(__dirname, 'public'))); // Specify the folder which holds static files
 app.use(bodyParser.json()); // Parse input text to JSON
 app.use(bodyParser.urlencoded({ extended: true })); // Ensure proper/safe URL encoding
+app.use(multer({ storage: multer.memoryStorage({}) }).any()); // Configure multer to hold uploaded file data in memory
 
 // Mount routes to the express app
 app.use(require('./routes/home_route'));
