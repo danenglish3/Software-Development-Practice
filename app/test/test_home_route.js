fdb97e68bb2fb6842908111d1cd58f304c079012
Suved <dwn2786@autuni.ac.nz>
2018-09-06 21:13:30 +1200

    Cleaned up home route and it's test.

--- app/test/test_home_route.js
+++ app/test/test_home_route.js
@@ -13,8 +13,14 @@ describe('Home Route', () => {
     // Start the server before running any tests
     before((done) => { // Pass done to tell mocha to wait until done() is called
         app = express(); // Initialise express
+
         // Set view engine to EJS
         app.set('view engine', 'ejs');
+        app.set('views', path.join(__dirname, '../views'));
+
+        // Provide Express with middleware
+        app.use(express.static(path.join(__dirname, 'public'))); // Specify the folder which holds static files
+
         server = app.listen(3000, () => { // Start the server on port 3000 while mocha is waiting
             done(); // Tell mocha we are done so it no longer has to wait
         });
@@ -30,16 +36,15 @@ describe('Home Route', () => {
         // Get express's router functions
         const router = express.Router();
         // Get the path to index.html
-        const filePath = path.join(__dirname, '../views/index.ejs');
         // Read file at the specified path into a string (reading synchonously is OK for testing)
-        const file = fs.readFileSync(filePath, { encoding: 'utf-8' }, (err, contents) => contents);
+        const file = fs.readFileSync(path.join(__dirname, '../views/index.ejs'), { encoding: 'utf-8' }, (err, contents) => contents);
         // Render view with EJS
         const homepage = ejs.render(file, { siteName: 'Kiwi Trader' });
 
         // Respond to supertest's GET request with the file at filePath
         request(router)
             .get('/', (req, res) => {
-                res.render(filePath, { siteName: 'Kiwi Trader' });
+                res.render('index.ejs', { siteName: 'Kiwi Trader' });
             })
             // Then compare the contents of the file we read with
             // the file we sent to ensure they are both the same
