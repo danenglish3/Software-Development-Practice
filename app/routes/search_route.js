390228f9a954edcd05b92397dda878353fa50b25
Daniel English <mps0494@autuni.ac.nz>
2018-09-12 13:52:18 +1200

    Added more to the search by keyword

--- app/routes/search_route.js
+++ app/routes/search_route.js
@@ -143,31 +143,134 @@ router.get('/search/category/:category', (req, res) => {
     });
 });
 
-// Search by just a keyword -- url example, http://localhost:3000/search/keyword?key=Carl+Dan
-router.get('/search/keyword', (req) => {
-    // let listingResults = [];
+router.get('/search/keyword', (req, res) => {
+    let listingResults = [];
     let serviceIdResults = [];
-    // let count = 0;
+    let count = 0;
     let parameterArray = req.query.key.split(' '); // Turn the parameter into an array of strings
     // console.log(parameterArray);
-    parameterArray.forEach((element) => { // Search for Titles containing the keywords
-        const queryService = `SELECT * FROM website_user.Service \
+    const switchCase = 0;
+
+    switch (switchCase) {
+    case 0:
+        parameterArray.forEach((element) => { // Search for Titles containing the keywords
+            const queryService = `SELECT * FROM website_user.Service \
         WHERE Title LIKE '%${element}%'`; // Submit the statement
-        connection.query(queryService, (err, results) => {
-        // Once the above query is complete:
-            if (err) throw err;
-            // console.log(results);
-            if (results.length > 0) {
-                results.forEach((service) => {
-                    const singleID = {
-                        serviceid: service.Service_ID,
-                    };
-                    serviceIdResults.push(singleID);
-                });
-            }
-            console.log(serviceIdResults);
+            connection.query(queryService, (err, results) => {
+                // Once the above query is complete:
+                if (err) throw err;
+                // console.log(results);
+                if (results.length > 0) {
+                    results.forEach((service) => {
+                        const singleID = {
+                            serviceid: service.Service_ID,
+                        };
+                        serviceIdResults.push(singleID);
+                    });
+                }
+                // console.log('Title', serviceIdResults);
+            });
         });
-    });
+    case 1:
+        parameterArray.forEach((element) => { // Search for Titles containing the keywords
+            const queryService = `SELECT * FROM website_user.Service \
+                WHERE Description LIKE '%${element}%'`; // Submit the statement
+            connection.query(queryService, (err, results) => {
+            // Once the above query is complete:
+                if (err) throw err;
+                // console.log(results);
+                if (results.length > 0) {
+                    results.forEach((service) => {
+                        const singleID = {
+                            serviceid: service.Service_ID,
+                        };
+                        serviceIdResults.push(singleID);
+                    });
+                }
+                // console.log('Description', serviceIdResults);
+            });
+        });
+    case 2:
+        parameterArray.forEach((element) => { // Search for Titles containing the keywords
+            const queryService = `SELECT * FROM website_user.Service \
+                WHERE Location LIKE '%${element}%'`; // Submit the statement
+            connection.query(queryService, (err, results) => {
+            // Once the above query is complete:
+                if (err) throw err;
+                // console.log(results);
+                if (results.length > 0) {
+                    results.forEach((service) => {
+                        const singleID = {
+                            serviceid: service.Service_ID,
+                        };
+                        serviceIdResults.push(singleID);
+                    });
+                }
+                console.log('Location', serviceIdResults);
+            });
+        });
+        const queryServices = `SELECT * FROM website_user.Service WHERE \
+        website_user.Service.Service_ID IN ( ${1008} )`;
+        connection.query(queryServices, (err7, results5) => {
+            if (err7) throw err7;
+            console.log('Services', results5);
+            results5.forEach((service) => {
+                // Step 2) Create a statement to get the profile for which the service is linked to
+                const queryProfileID = `SELECT Account_ID FROM website_user.Profile WHERE \
+                        website_user.Profile.Profile_ID = '${service.Profile_ID}'`;
+                connection.query(queryProfileID, (err2, results2) => {
+                    if (err2) throw err2;
+                    // Once the above is complete
+                    const accID = {
+                        accid: results2[0].Account_ID,
+                    };// Save the account ID for easy reference (was used in earlier versions)
+
+                    // step 3) Create a statement that will get the business or persons name that holds the account
+                    //         using the account id selected in step 2
+                    const queryAccountName = `SELECT Name FROM website_user.AccountHolder WHERE \
+                            website_user.AccountHolder.Account_ID = '${accID.accid}'`;
+                    connection.query(queryAccountName, (err3, results3) => {
+                        if (err3) throw err3;
+
+                        // Step 4) Create a statement that will gather all the photos that are related to a aervice
+                        const queryPhotos = `SELECT * FROM Photo WHERE Service_ID = ${service.Service_ID}`;
+                        connection.query(queryPhotos, (err4, results4) => {
+                            // Create a singleListing object that holds the information required
+                            const singleListing = {
+                                serviceid: service.Service_ID,
+                                name: results3[0].Name, // Selected in step 3
+                                location: service.Location,
+                                category: service.Category,
+                                description: service.Description,
+                                imageFiles: [], // Create empty array for holding filenames for the images
+                            };
+                            results4.forEach((photo) => { // For each result of the photo query (results3):
+                                const filename = `${uuid()}.${photo.Extension}`; // Create a filename
+                                singleListing.imageFiles.push(filename); // Add filename to array in singleListing object
+                                // Write the file to the temp directory
+                                fs.writeFile(`app/public/temp/${filename}`, photo.Photo_Blob, (err5) => {
+                                    if (err5) throw err5;
+                                });
+                            });
+
+                            listingResults.push({ singleListing });
+                            count += 1;
+                            if (count === results5.length) { // Once all the services has been looped through and added
+                                console.log(listingResults);
+                                res.render('search.ejs', { listingResults });
+                                // Must be rendered in this step otherwise it wont work for some reason..
+                            }
+                        });
+                    });
+                });
+            });
+        });
+        break;
+    default:
+        console.log('hit defaul..');
+    }
+
+
     // parameterArray.splice(invalidIndex[0], 1);
     // console.log(invalidIndex);
 });
