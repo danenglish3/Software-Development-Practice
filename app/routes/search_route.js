1942860ccca70fc1b9f3f2810bd39eb2dd9e6790
Daniel English <mps0494@autuni.ac.nz>
2018-09-14 15:06:38 +1200

    Added in new search results page, updated comments

--- app/routes/search_route.js
+++ app/routes/search_route.js
@@ -3,74 +3,73 @@ const express = require('express');
 const fs = require('fs');
 const uuid = require('uuid/v4');
 const connection = require('../database');
-var session = require('express-session')
+var session = require('express-session') // Used to save data between function calls
 
 const router = express.Router(); // Get express's router functions
-router.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}));
+router.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }})); // Initialize secret? 
 
-router.get('/search', (req, res) => {
-    let listingResults = [];
-    const singleListing = {
-        serviceid: 0,
-        name: 'asd ', // Selected in step 3
-        location: ' asd',
-        category: ' asd',
-        description: ' asd',
-        imageFiles: [], // Create empty array for holding filenames for the images
-    };
-    listingResults.push(singleListing);
-    res.render('search.ejs', { listingResults });
+router.get('/search', (req, res) => { //Initial setup for search page
+    res.render('search.ejs');
 });
 
+// Post call for after a new search form has been submitted.
+// Will goto findServices THEN renderSearchPage
 router.post('/search', findServices, renderSearchPage, (req, res) => {});
 
+// First call after the .post from a new Search
 function findServices(req, res, next) {
     // console.log(req);
-    const searchParamaters = {
+    const searchParamaters = { // Get search params from the form
         location: req.body.searchLocation,
         category: req.body.searchCategory,
         keywords: req.body.searchKeywords,
     };
-
+    let needJoin = false; // Indicate if their is more than one search param
+    if(!(searchParamaters.location === 'Default') && !(searchParamaters.category === 'Default')){
+        needJoin = true;
+    }
     console.log('search params', searchParamaters);
-    if(searchParamaters.location === undefined){
-        searchParamaters.location = 'Test'
-        console.log('changed location to test');
+
+    let condition = []; // Search conditions 
+    if(!(searchParamaters.location === 'Default')){ // IF search by location, add to conditions
+        const queryLocationService = `Location='${searchParamaters.location}'`;
+        condition.push(queryLocationService);
     }
-    if(searchParamaters.category === undefined){
-        searchParamaters.category = 'Test'
+    if(!(searchParamaters.category === 'Default')){ // If search by category, add to conditions
+        const queryCategoryService = `Category='${searchParamaters.category}'`;
+        condition.push(queryCategoryService);
     }
 
-    let condition = [];
-    const queryLocationService = `Location='${searchParamaters.location}'`;
-    condition.push(queryLocationService);
-    const queryCategoryService = `Category='${searchParamaters.category}'`;
-    condition.push(queryCategoryService);
-    condition.join(' AND ');
-
-    let conditionString = condition.join(' AND ');
+    let conditionString; // Condition string for upcoming DB query
+    console.log('condition ', condition);
+    if(needJoin){
+        conditionString = condition.join(' AND '); // If multiple conditions, join with AND
+    }    
+    else{
+        conditionString = condition[0];
+    }
     console.log(conditionString);
-    let sql = `SELECT * FROM website_user.Service WHERE ${conditionString}`;
+    let sql = `SELECT * FROM website_user.Service WHERE ${conditionString}`; // SQL query for getting srvices
     console.log(sql);
     connection.query(sql, (err, results) => {
         console.log(results);
-        req.services = results;
-        return next();
+        req.services = results; // Save service results in req to be used in next function
+        return next(); // Call next function, renderSearchPage
     });
 }
 
 function renderSearchPage(req, res) {
-    let listingResults = [];
-    let count = 0;
-    // console.log('req length', req.services.length);
-    req.services.forEach((service) => {
-        // Step 4) Create a statement that will gather all the photos that are related to a aervice
+    let listingResults = []; // Final listing results array
+    let count = 0; // Count of how many results there are
+
+    req.services.forEach((service) => { //For each service found in the previous function
+        // Create a statement that will gather all the photos that are related to a aervice
         const queryPhotos = `SELECT * FROM Photo WHERE Service_ID = ${service.Service_ID}`;
         connection.query(queryPhotos, (err4, results4) => {
             // Create a singleListing object that holds the information required
             const singleListing = {
                 serviceid: service.Service_ID,
-                name: service.Title, // Selected in step 3
+                name: service.Title,
                 location: service.Location,
                 category: service.Category,
                 description: service.Description,
@@ -84,25 +83,25 @@ function renderSearchPage(req, res) {
                     if (err5) throw err5;
                 });
             });
-            listingResults.push({ singleListing });
-            // console.log('listing results', listingResults);
-            count += 1;
+            listingResults.push({ singleListing }); // Push completed lising into final array
+            count += 1; //update count
             if (count === req.services.length) { // Once all the services has been looped through and added
-                console.log('hit render ', listingResults);
+                // console.log('hit render ', listingResults);
                 // res.render('search.ejs', { listingResults: listingResults });
-                req.session.serviceResults = listingResults;
-                res.redirect('/search/results');
-                // Must be rendered in this step otherwise it wont work for some reason..
+                req.session.serviceResults = listingResults; // Save listing results to be used by next function
+                res.redirect('/search/results'); // Redirect user to a new page
             }
         });
     });
 }
 
+// This is used to render a new search results page after geting all the results
 router.get('/search/results', (req, res) => {
-    let listingResults =  req.session.serviceResults;
-    console.log('search/results', listingResults);
-    res.render('search-results.ejs', {listingResults});
+    let listingResults =  req.session.serviceResults; // Results saved in previous function
+    // console.log('search/results', listingResults);
+    res.render('search-results.ejs', {listingResults}); // Render the new page
 });
+
 // Allow the router object to be used in other js files.
 // In this case we are making the router available as a
 // module so that the route to the home page can be
