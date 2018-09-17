06d039b2d1aa73d0cdc41327f7cbc0ffe4ca6503
Daniel English <mps0494@autuni.ac.nz>
2018-09-17 16:02:02 +1200

    Added in checks for no services returned

--- app/routes/search_route.js
+++ app/routes/search_route.js
@@ -14,7 +14,6 @@ router.get('/search', (req, res) => { // Initial setup for search page
 
 // First call after the .post from a new Search
 function findServices(req, res, next) {
-    // console.log(req);
     const searchParamaters = { // Get search params from the form
         location: req.body.searchLocation,
         category: req.body.searchCategory,
@@ -23,15 +22,15 @@ function findServices(req, res, next) {
     let needJoin = false; // Indicate if their is more than one search param
     if (!(searchParamaters.location === 'Default') && !(searchParamaters.category === 'Default')) {
         needJoin = true;
-    }
-    else if (!(searchParamaters.location === 'Default') && !(searchParamaters.keywords === '')) {
+    } else if (!(searchParamaters.location === 'Default') && !(searchParamaters.keywords === '')) {
+        needJoin = true;
+    } else if (!(searchParamaters.category === 'Default') && !(searchParamaters.keywords === '')) {
         needJoin = true;
     }
-    else if (!(searchParamaters.category === 'Default') && !(searchParamaters.keywords === '')) {
-        needJoin = true;
+    req.undefResults = false;
+    if ((searchParamaters.location === 'Default') && (searchParamaters.category === 'Default') && (searchParamaters.keywords === '')) {
+        req.undefResults = true;
     }
-    console.log('search params', searchParamaters);
-
     let condition = []; // Search conditions
     if (!(searchParamaters.location === 'Default')) { // IF search by location, add to conditions
         const queryLocationService = `Location='${searchParamaters.location}'`;
@@ -41,13 +40,11 @@ function findServices(req, res, next) {
         const queryCategoryService = `Category='${searchParamaters.category}'`;
         condition.push(queryCategoryService);
     }
-    // let keywordStringArray = [];
     let keywordString;
+    let keywordStringArray = [];
     if (!(searchParamaters.keywords === '')) { // If search by keywords, add them to conditions
-        let keywordStringArray = [];
         keywordStringArray = searchParamaters.keywords.split(' ');
-        // console.log('array length' ,keywordStringArray.length);
-        // console.log('keyword array: ', keywordStringArray);
+
         for (let i = 0; i < keywordStringArray.length; i += 1) {
             keywordString += keywordStringArray[i];
             if (!(i === keywordStringArray.length - 1)) {
@@ -55,11 +52,9 @@ function findServices(req, res, next) {
             }
         }
         keywordString = keywordString.slice(9, keywordString.length);
-        // console.log('keyword string: ', keywordString);
-        const queryDesc = `Description REGEXP '${keywordString}'`;
+        const queryDesc = `Description REGEXP '${keywordString}' OR Title REGEXP '${keywordString}'`;
         condition.push(queryDesc);
     }
-
     let conditionString; // Condition string for upcoming DB query
     console.log('condition ', condition);
     if (needJoin) {
@@ -67,11 +62,8 @@ function findServices(req, res, next) {
     } else {
         conditionString = condition[0];
     }
-    console.log(conditionString);
     let sql = `SELECT * FROM website_user.Service WHERE ${conditionString}`; // SQL query for getting srvices
-    console.log(sql);
     connection.query(sql, (err, results) => {
-        console.log(results);
         req.services = results; // Save service results in req to be used in next function
         return next(); // Call next function, renderSearchPage
     });
@@ -80,38 +72,43 @@ function findServices(req, res, next) {
 function renderSearchPage(req, res) {
     let listingResults = []; // Final listing results array
     let count = 0; // Count of how many results there are
-
-    req.services.forEach((service) => { // For each service found in the previous function
+    if (!(req.undefResults) && req.services.length === 0) {
+        req.undefResults = true;
+    }
+    if (!(req.undefResults)) {
+        req.services.forEach((service) => { // For each service found in the previous function
         // Create a statement that will gather all the photos that are related to a aervice
-        const queryPhotos = `SELECT * FROM Photo WHERE Service_ID = ${service.Service_ID}`;
-        connection.query(queryPhotos, (err4, results4) => {
+            const queryPhotos = `SELECT * FROM Photo WHERE Service_ID = ${service.Service_ID}`;
+            connection.query(queryPhotos, (err4, results4) => {
             // Create a singleListing object that holds the information required
-            const singleListing = {
-                serviceid: service.Service_ID,
-                name: service.Title,
-                location: service.Location,
-                category: service.Category,
-                description: service.Description,
-                imageFiles: [], // Create empty array for holding filenames for the images
-            };
-            results4.forEach((element) => { // For each result of the photo query (results3):
-                const filename = `${uuid()}.${element.Extension}`; // Create a filename
-                singleListing.imageFiles.push(filename); // Add filename to array in singleListing object
-                // Write the file to the temp directory
-                fs.writeFile(`app/public/temp/${filename}`, element.Photo_Blob, (err5) => {
-                    if (err5) throw err5;
+                const singleListing = {
+                    serviceid: service.Service_ID,
+                    name: service.Title,
+                    location: service.Location,
+                    category: service.Category,
+                    description: service.Description,
+                    imageFiles: [], // Create empty array for holding filenames for the images
+                };
+                results4.forEach((element) => { // For each result of the photo query (results3):
+                    const filename = `${uuid()}.${element.Extension}`; // Create a filename
+                    singleListing.imageFiles.push(filename); // Add filename to array in singleListing object
+                    // Write the file to the temp directory
+                    fs.writeFile(`app/public/temp/${filename}`, element.Photo_Blob, (err5) => {
+                        if (err5) throw err5;
+                    });
                 });
+                listingResults.push({ singleListing }); // Push completed lising into final array
+                count += 1; // update count
+                if (count === req.services.length) { // Once all the services has been looped through and added
+                    req.session.serviceResults = listingResults; // Save listing results to be used by next function
+                    res.redirect('/search/results'); // Redirect user to a new page
+                }
             });
-            listingResults.push({ singleListing }); // Push completed lising into final array
-            count += 1; // update count
-            if (count === req.services.length) { // Once all the services has been looped through and added
-                // console.log('hit render ', listingResults);
-                // res.render('search.ejs', { listingResults: listingResults });
-                req.session.serviceResults = listingResults; // Save listing results to be used by next function
-                res.redirect('/search/results'); // Redirect user to a new page
-            }
         });
-    });
+    } else {
+        req.session.serviceResults = [];
+        res.redirect('/search/results');
+    }
 }
 
 // Post call for after a new search form has been submitted.
