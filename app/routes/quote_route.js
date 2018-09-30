5fe1305e97f837fff259820b56a0d632c86f9166
Daniel English <mps0494@autuni.ac.nz>
2018-10-01 09:49:55 +1300

    added button from search to new quote page

--- app/routes/quote_route.js
+++ app/routes/quote_route.js
@@ -1,10 +1,15 @@
 const express = require('express');
+
 const router = express.Router(); // Get express's router functions
 
 function renderQuotePage(req, res) {
+    const listingDetails = {
+        serviceID: req.params.id,
+    };
+    console.log(`profile ID : ${listingDetails.profileID}`);
     res.render('new_quote.ejs');
 }
 
-router.get('/new_quote', renderQuotePage, (req, res) => {});
+router.get('/new_quote/:id', renderQuotePage, (req, res) => {});
 
 module.exports = router;
