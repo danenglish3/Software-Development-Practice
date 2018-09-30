b074606c6c8ddba24cc6ba450975942dbd74cb3a
Daniel English <mps0494@autuni.ac.nz>
2018-10-01 10:20:54 +1300

    added in db calls to get service

--- app/routes/quote_route.js
+++ app/routes/quote_route.js
@@ -1,15 +1,33 @@
 const express = require('express');
+const connection = require('../database');
 
 const router = express.Router(); // Get express's router functions
 
-function renderQuotePage(req, res) {
-    const listingDetails = {
-        serviceID: req.params.id,
-    };
-    console.log(`profile ID : ${listingDetails.profileID}`);
-    res.render('new_quote.ejs');
+function getService(req, res, next) {
+    const queryService = `SELECT * FROM website_user.Service WHERE Service_ID=${req.params.id}`;
+    console.log(req.params.id);
+    connection.query(queryService, (err, results) => {
+        if (err) {
+            next(err);
+        } else if (!results.length) {
+            console.log('hit else');
+            next(new Error('404'));
+        } else {
+            req.service = {
+                serviceName: results[0].Title,
+                prevProfile: results[0].Profile_ID,
+            };
+            console.log('hit else');
+        }
+        next();
+    });
 }
 
-router.get('/new_quote/:id', renderQuotePage, (req, res) => {});
+function renderQuotePage(req, res) {
+    console.log(req.service);
+    res.render('new_quote.ejs', req.service);
+}
+
+router.get('/new_quote/:id', getService, renderQuotePage, (req, res) => {});
 
 module.exports = router;
