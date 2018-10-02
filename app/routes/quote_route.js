0fb90089a570fbf47c7234b11a1cf197ee2ed4f4
Daniel English <mps0494@autuni.ac.nz>
2018-10-02 13:59:02 +1300

    quote now gets user account from whoever is logged in

--- app/routes/quote_route.js
+++ app/routes/quote_route.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const connection = require('../database');
 
 const router = express.Router(); // Get express's router functions
 
+// Get the service details for the desired quote
 function getService(req, res, next) {
     const queryService = `SELECT * FROM website_user.Service WHERE Service_ID=${req.params.id}`;
     console.log(req.params.id);
@@ -10,37 +12,49 @@ function getService(req, res, next) {
         if (err) {
             next(err);
         } else if (!results.length) {
-            console.log('hit else');
             next(new Error('404'));
         } else {
+            // Create a new service object
             req.service = {
                 serviceID: req.params.id,
                 serviceName: results[0].Title,
                 profileID: results[0].Profile_ID,
             };
-            // console.log('hit else');
         }
-        next();
+        next(); // Call next func, renderQuotePage
     });
 }
 
-function renderQuotePage(req, res) {
+// This function is called after getService. It will render the new quote page
+function renderQuotePage(req, res) { 
     console.log(req.service);
     res.render('new_quote.ejs', req.service);
 }
 
+// Get for displaying new quote page, will call 2 functions
 router.get('/new_quote/:id', getService, renderQuotePage, (req, res) => {});
 
+// This function is called when the post request for new quote is sent
 function insertQuote(req, res, next) {
-    const quoteDetails = {
+    let userAccID; // User account ID needed to be saved in quote
+    const userSession = req.cookies.SessionInfo; // Get the current user session details
+    jwt.verify(userSession, 'dcjscomp602', (err, decoded) => { // Decode the session
+        if (err) { // Return error 500, error on code side if function doesn't successfully call
+            next(new Error('500'));
+        } else {
+            userAccID = decoded.data.Account_ID; // Sace current user account ID
+            console.log(decoded.data);
+        }
+    });
+    const quoteDetails = { // Create new Quote object
         serviceRequestedID: req.body.serviceID,
-        accountRequestedFromID: 12351,
+        accountRequestedFromID: userAccID,
         phoneNumber: req.body.quoteContact,
         emailAddress: req.body.quoteEmail,
         description: req.body.quoteDesc,
     };
-    console.log(quoteDetails);
-    const insertQuoteStmnt = 'INSERT INTO Quote SET ?';
+    // console.log(quoteDetails);
+    const insertQuoteStmnt = 'INSERT INTO Quote SET ?'; // Save into the DB
     connection.query(insertQuoteStmnt, quoteDetails, (err, results) => {
         if (err) {
             next(err);
